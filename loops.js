18db1cb2ebd085d41ea91ab063edb8bea0631935
Akhila-web <akhilareddycholleti@gmail.com>
2024-10-23 23:33:59 -0500

    Add files via upload

---
+++ loops.js
@@ -0,0 +1,62 @@
+// for loop
+for(var a=1;a<=3;a++){
+    console.log(a)
+}
+// example2
+for(var a=11;a>=6;a--){
+    console.log(a)
+}
+// example3
+for(var i=0;i<=5;i++){
+
+}
+console.log(i)
+
+// whileloop
+// example1
+var a=1;
+while(a<=7){
+    console.log(a)
+    a++
+}
+// example2
+var a=1;
+while(a<3){
+    console.log(a)
+    a++
+}
+// example3
+var a=10;
+while (a>=6){
+    console.log(a)
+    a--
+}
+// example4
+var i=1
+while(i<4){
+    i++
+}
+console.log(i)
+
+// do while loop
+// example1
+var a=1;
+do{
+    a++
+    console.log(a)
+}while(a<=5)
+
+    // example2
+    var a=10;
+    do{
+        console.log(a)
+        a--
+    }while(a>=7)
+        // example3
+        var a=10;
+        do{
+            a--
+            console.log(a)
+        }while(a>=7)
+    
+        
